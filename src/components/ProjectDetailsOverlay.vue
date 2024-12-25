cae3717d01078da13d5d42dcbce0bfcd3a33d14d
Aviv <avivhauyn@gmail.com>
2024-12-25 16:34:27 +0200

    Updated project overlay
    Updated and added needed CSS
    Added support for code blocks with highlighting

--- src/components/ProjectDetailsOverlay.vue
+++ src/components/ProjectDetailsOverlay.vue
@@ -5,11 +5,7 @@
       <div class="overlay" @click="$emit('close')"></div>
 
       <!-- Dialog Window -->
-      <div
-        class="dialog"
-        :style="{ 'background-color': color }"
-        @click.stop
-      >
+      <div class="dialog" :style="{ 'background-color': color }" @click.stop>
         <!-- Title -->
         <h1 class="dialog-title">{{ title }}</h1>
 
@@ -35,6 +31,8 @@
 
 <script lang="ts">
 import Vue from "vue";
+import hljs from "highlight.js"; // Import Highlight.js library
+import "highlight.js/styles/obsidian.css"; // Import a Highlight.js theme
 
 export default Vue.extend({
   name: "ProjectDetailsOverlay",
@@ -44,45 +42,63 @@ export default Vue.extend({
     title: String,
     htmlContent: String,
   },
+  mounted() {
+    this.highlightCode();
+  },
+  updated() {
+    this.highlightCode();
+  },
   methods: {
-    getImage: function(url: string) {
+    highlightCode() {
+      const blocks = this.$el.querySelectorAll("pre.code-snippet");
+      blocks.forEach((block) => {
+        hljs.highlightElement(block as HTMLElement);
+      });
+    },
+    getImage(url: string) {
       console.log("fetching image " + url);
-    }
-  }
+    },
+  },
 });
+
 </script>
+
 
 <style scoped>
 .overlay {
-  background-color: rgba(0,0,0,0.5);
+  background-color: rgba(0, 0, 0, 0.5);
   z-index: 10;
-  position:fixed;
-  top:0px;
-  left:0px;
-  right:0px;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
   bottom: 0px;
 }
 
 .dialog {
-  position:absolute;
+  position: absolute;
   top: 0px;
   left: 0px;
   right: 0px;
   z-index: 11;
   margin: 20px;
   padding-bottom: 10px;
-  color:white;
+  color: white;
+  border-radius: 4px;
+  box-shadow: inset -1px 2px 5px 0px rgba(0, 0, 0, 0.45);
 }
+
+
 
 iframe {
   width: 100%;
 }
 
 h1.dialog-title {
-    text-align: center;
-    font-size: 1.3em;
-    margin: 0px;
-    padding: 22px;
+  text-align: center;
+  font-size: 1.3em;
+  margin: 0px;
+  padding: 22px;
 }
 
 .dialog-content {
@@ -90,17 +106,19 @@ h1.dialog-title {
 }
 
 .dialog-content {
-  background-color: #fcfcfc;
-  color: #696969;
+  background-color: #17191b;
+  color: #cacaca;
 }
+
 .dialog-close {
   position: absolute;
   top: 20px;
   right: 20px;
-  cursor:pointer;
+  cursor: pointer;
   font-size: 1.2em;
   font-weight: 100;
 }
+
 .dialog-close:hover {
   opacity: 0.6;
 }
@@ -110,13 +128,13 @@ h1.dialog-title {
 }
 
 a.dialog-close-button {
-  cursor:pointer;
+  cursor: pointer;
   font-size: 1.4em;
   display: inline-block;
   margin: 0 auto;
 }
 
-@media only screen and (min-width: 620px){
+@media only screen and (min-width: 620px) {
   .dialog {
     margin: 0 auto;
     margin-top: 80px;
@@ -132,6 +150,4 @@ a.dialog-close-button {
     padding: 40px;
   }
 }
-
-
 </style>
