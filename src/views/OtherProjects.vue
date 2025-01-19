0871962140996cd9c7fa64a787ca85d478533833
Aviv <avivhauyn@gmail.com>
2025-01-19 19:37:27 +0200

    Clean up and centering

--- src/views/OtherProjects.vue
+++ src/views/OtherProjects.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <h1>Tech & Tools</h1>
+    <h1 class="centered">Tech & Tools</h1>
 
     <div style="margin-bottom: 30px;">
       Explore the Unity tools I’ve developed, in-depth overviews of different systems i developed.
       This section highlights my technical expertise and contributions to game development.
-      <h1 style="line-height: 1.7; text-align: center;"><strong>NOTICE</strong> - This area of the website is still under development come back later to see what i add :)</h1> 
     </div>
     <ProjectsList v-bind:projects="projects" />
+    <div style="margin-bottom: 30px;">
+      <h2 style="line-height: 1.7; text-align: center;"><strong>NOTICE</strong> - This area of the website is still under
+        development come back later to see what i add :)</h2>
+    </div>
   </div>
 </template>
 
@@ -30,3 +33,13 @@ export default Vue.extend({
   },
 });
 </script>
+<style scoped>
+.centered {
+  text-align: center;
+}
+
+h1.centered {
+  margin: 20px;
+  font-weight: bold;
+}
+</style >
